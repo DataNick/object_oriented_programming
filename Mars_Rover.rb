3c66fae4d4c183dd19a767b23a172d5c476b2790
DataNick <DataNick@users.noreply.github.com>
2014-10-23 14:58:50 -0400

    movement of MarsRover

--- Mars_Rover.rb
+++ Mars_Rover.rb
@@ -1,6 +1,23 @@
 class MarsRover
 	attr_accessor :x, :y
 
-	def initialize(*args)
-		@x, @y = args
+	def initialize(x, y, dir)
+		@x = x
+		@y = y
+		@dir = dir
+	end
+
+	def position
+		puts "I am at #{x}, #{y} heading #{dir}."
+	end
+
+	def move
+
+	end
+
+	def right
+	end
+
+	def left
+	end
 end
