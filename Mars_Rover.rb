4cdf8a4e4158a8168755846e7e83ef45dc948f7c
DataNick <DataNick@users.noreply.github.com>
2014-10-23 16:17:12 -0400

    Breakout session

--- Mars_Rover.rb
+++ Mars_Rover.rb
@@ -1,38 +1,62 @@
 class MarsRover
-	attr_accessor :x, :y, :dir
-
-	def initialize(x, y, dir)
+	attr_accessor :x, :y, :direction
+# to acces the instance variables outside class, we need attr_accessor to receive those 
+	def initialize(x, y, direction)
 		@x = x
 		@y = y
-		@dir = dir
+		@direction = direction
 	end
 
-	def position
-		puts "I am at #{x}, #{y} heading #{dir}."
+	def read_instruction(instruction)
+		if instruction == "M"
+			move
+		elsif instruction == "L"
+			turn_left
+		elsif instruction == "R"
+			turn_right
+		end
 	end
+
+	def move
+		if @direction == "N"
+			@y += 1
+		elsif @dir = "S"
+			@y -= 1
+		elsif @dir = "E"
+			@x += 1
+		elsif @dir = "W"
+			@x -= 1
+		end
+	end
+	
+	# def position
+	# 	puts "I am at #{x}, #{y} heading #{dir}."
+	# end
 
 # if rover moves North (up) the y variable is +. If it goes East 
 # (left), the rover x variable is -.
-the  
 
-	def move
-		if @dir = "N"
-			then @y += 1
-		elsif @dir = "S"
-			then @y -= 1
-		elsif @dir = "E"
-			then @x += 1
-		elsif @dir = "W"
-			then @x -= 1
-		end
-	end
+	
 			
 
-	end
+	# def right(dir)
+	# 	if (@direction == "N") && (@instruction == "L")
+	# 		@direction == "W"
+	# 	elsif (@direction == "S")
+			
+	# end
 
-	def right
-	end
-
-	def left
-	end
+	# def left(dir)
+	#end
 end
+rover = MarsRover.new(0,0,"N")
+
+puts "Rover's current position is #{rover.x}, #{rover.y}, facing #{rover.direction}"
+puts rover.read_instruction("M")
+puts "Rover's current position is #{rover.x}, #{rover.y}, facing #{rover.direction}"
+
+
+
+
+
+
